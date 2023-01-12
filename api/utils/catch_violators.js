3fddef2a183d6894031ad01e0fc85a33fb2256d6
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-01-13 00:47:57 +0200

    Added calculating which drones are in the no drone zone

---
+++ api/utils/catch_violators.js
@@ -0,0 +1,33 @@
+const calculateDistance = (positionX, positionY) => {
+    const ndzX = 250000;
+    const ndzY = 250000;
+
+    const distance = Math.sqrt(Math.pow((positionX-ndzX), 2) + Math.pow((positionY-ndzY), 2));
+
+    return distance;
+}
+
+const inNDZ = (drone) => {
+    if (drone.distance <= 100000) {
+        return true;
+    }
+    return false;
+}
+
+const mapDrone = (drone) => {
+    return {
+        serialNumber: drone.serialNumber._text,
+        distance: calculateDistance(drone.positionX._text, drone.positionY._text)
+    }
+}
+
+// returns list with serialNumber and distance
+const catchViolators = (droneList) => {
+    const mappedDrones = droneList.map(mapDrone);
+    console.log(mappedDrones);
+    const filteredDrones = mappedDrones.filter(inNDZ);
+    console.log(filteredDrones);
+    return filteredDrones;
+}
+
+module.exports = catchViolators;
