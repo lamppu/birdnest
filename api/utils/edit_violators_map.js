4ca88c05ab2bb2af7f001f6b6348a1050b497365
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-01-18 15:44:44 +0200

    Moved fetchPilot to its own module, edited comments

--- api/utils/edit_violators_map.js
+++ api/utils/edit_violators_map.js
@@ -1,29 +1,12 @@
-const fetchData = require('./fetch_data.js');
-const Violator = require("../classes/violator");
-const Pilot = require("../classes/pilot");
-
-const fetchPilot = async (serialNumber) => {
-    const pilotData = await fetchData("https://assignments.reaktor.com/birdnest/pilots/" + serialNumber, "JSON");
-
-    if (pilotData.status === 200) {
-        const name = pilotData.data.firstName + " " + pilotData.data.lastName;
-        return {
-            status: pilotData.status,
-            pilot: new Pilot(name, pilotData.data.email, pilotData.data.phoneNumber)
-        }
-    }
-    return {
-        status: pilotData.status,
-        pilot: new Pilot(null, null, null)
-    }
-}
-
+const Violator = require('../classes/violator');
+const fetchPilot = require('./fetch_pilot');
 
 const addViolators = async (violatorsMap, caughtViolators, timeStamp, unsuccessfulFetchArr) => {
 
     caughtViolators.forEach(async violator => {
         
         if (violatorsMap.has(violator.serialNumber)) {
+            //drones that are already in the violatorsMap
 
             const violatorMapObj = violatorsMap.get(violator.serialNumber);
 
@@ -31,21 +14,22 @@ const addViolators = async (violatorsMap, caughtViolators, timeStamp, unsuccessf
                 violatorMapObj.closestDistance = violator.distance;
             }
             violatorMapObj.timeStamp = timeStamp;
+            violatorMapObj.ndzStatus = violator.inNDZ;
 
             violatorsMap.set(violator.serialNumber, violatorMapObj);
         } else {
-            
+            //new violators
+
             const pilotData = await fetchPilot(violator.serialNumber);
+            
             if (pilotData.status !== 200) {
                 unsuccessfulFetchArr.push(violator.serialNumber);
             }
             
-            violatorsMap.set(violator.serialNumber, new Violator(pilotData.pilot, violator.distance, timeStamp));
+            violatorsMap.set(violator.serialNumber, new Violator(pilot, violator.inNDZ, violator.distance, timeStamp));
 
         }
     });
-    console.log("After----------------------------------------");
-    console.log([...violatorsMap.entries()]);
 }
 
 module.exports = addViolators;
