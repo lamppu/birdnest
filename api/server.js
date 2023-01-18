5d62c2bb98e4c2fbae326d9f2622c9f41d893c6e
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-01-18 17:43:45 +0200

    Added production behaviour

--- api/server.js
+++ api/server.js
@@ -14,7 +14,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
+}
+
 app.get('/', (req, res) => {
+  
 })
 
 app.get('/stream', (req, res) => {
