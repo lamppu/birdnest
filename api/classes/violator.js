10e08aa8325033f3a98191355c7a9dec49981f90
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-01-18 14:16:29 +0200

    Added property ndzStatus

--- api/classes/violator.js
+++ api/classes/violator.js
@@ -1,6 +1,7 @@
 class Violator {
-    constructor(pilot, closestDistance, timeStamp) {
+    constructor(pilot, ndzStatus, closestDistance, timeStamp) {
         this.pilot = pilot;
+        this.ndzStatus = ndzStatus;
         this.closestDistance = closestDistance;
         this.timeStamp = timeStamp;
     }
